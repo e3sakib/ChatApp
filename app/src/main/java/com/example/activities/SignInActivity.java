8fcc611aa59c127bd946083dfe6a18eeb300aaaf
Sakib-Ahammed <satuhin6573@gmail.com>
2022-03-29 13:07:29 +0600

    2nd

--- app/src/main/java/com/example/activities/SignInActivity.java
+++ app/src/main/java/com/example/activities/SignInActivity.java
@@ -2,15 +2,24 @@ package com.example.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.R;
+import com.example.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private ActivitySignInBinding binding   ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding= ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(view ->
+                startActivity(new Intent(getApplicationContext(),SignInActivity.class)));
     }
 }

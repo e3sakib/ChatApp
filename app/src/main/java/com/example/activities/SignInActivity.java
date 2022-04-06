6caaaf350c215d7314e81dd4dadd81955e8b1891
sakib-ahammed <satuhin6573@gmail.com>
2022-04-07 01:17:12 +0600

    error solve in login

--- app/src/main/java/com/example/activities/SignInActivity.java
+++ app/src/main/java/com/example/activities/SignInActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+//import android.widget.Toast;
 
-import com.example.R;
+//import com.example.R;
+import com.example.activities.utilities.Constants;
+import com.example.activities.utilities.PreferenceManager;
 import com.example.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.FirebaseFirestore;
+
+//import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binding   ;
+    private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
         binding= ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -21,5 +34,92 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textCreateNewAccount.setOnClickListener(view ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+        //2. for sign check
+       // binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
+        binding.buttonSignIn.setOnClickListener(v ->{
+            if (isValidSignInDetails()) {
+
+                signIn();
+            }
+        });
     }
+
+    private void signIn(){
+      loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL,binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task ->{
+                    if (task.isSuccessful() && task.getResult() != null
+                    && task.getResult().getDocuments().size() > 0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else {
+                        loading(false);
+                        showToast("Unable to signIn");
+                    }
+                });
+    }
+
+    private void loading(Boolean isLoading){
+        if (isLoading){
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+
+    }
+
+    private Boolean isValidSignInDetails(){
+        if (binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+
+            showToast("Enter valid email");
+            return false;
+        }else if (binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter Password");
+            return false;
+        }else {
+            return false;
+        }
+    }
+
+
+
+
+
+    //2. for sign check
+//    private void addDataToFirestore(){
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String,Object> data = new HashMap<>();
+//        data.put("First_name", "sakib");
+//        data.put("Last_name","ahammed");
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//                    Toast.makeText(getApplicationContext(),"Data inserted",Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(exception ->{
+//                    Toast.makeText(getApplicationContext(),exception.getMessage(),Toast.LENGTH_SHORT).show();
+//                });
+//
+//    }
+
+
 }
--- app/src/main/java/com/example/activities/firebase/MessagingService.java
+++ app/src/main/java/com/example/activities/firebase/MessagingService.java
@@ -1,6 +1,6 @@
 package com.example.activities.firebase;
 
-import android.util.Log;
+//import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -11,12 +11,12 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-        Log.d("FCM","Token"+token);
+       // Log.d("FCM","Token"+token);
     }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.d("FCM","Message"+remoteMessage.getNotification().getBody());
+        //Log.d("FCM","Message"+remoteMessage.getNotification().getBody());
     }
 }

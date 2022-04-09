878dd2dc25c404c302537ee0ac28db7660bb255e
sakib-ahammed <satuhin6573@gmail.com>
2022-04-09 12:44:03 +0600

    Chat Screen Layout Design

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners(){
         binding.imageSignOut.setOnClickListener(v -> signOut() );
+        binding.fabNewChat.setOnClickListener(v->
+                startActivity(new Intent(getApplicationContext(),UsersActivity.class)));
     }
 
     private void loadUserDetails(){
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             preferenceManager.getString(Constants.KEY_USER_ID)
         );
         documentReference.update(Constants.KEY_FCM_TOKEN,token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+               // .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("unable to update token"));
     }
 
--- app/src/main/java/com/example/activities/models/User.java
+++ app/src/main/java/com/example/activities/models/User.java
@@ -1,2 +1,7 @@
-package com.example.activities.models;public class User {
+package com.example.activities.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name, image, email,token;
 }
